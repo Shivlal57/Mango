5321cdf2a761467b0ea130e7288a30b8bb2c42cd
Shivlal Bhattarai <sbmrc0321@gmail.com>
2025-08-25 23:00:22 +0545

    authAPI setup

---
+++ Mango.Service.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Service.AuthAPI;
+using Mango.Service.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Service.AuthAPI
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Mango.Service.AuthAPI/Program.cs
@@ -0,0 +1,54 @@
+using Mango.Service.AuthAPI;
+using Mango.Service.AuthAPI.Models;
+using Mango.Service.AuthAPI.Service;
+using Mango.Service.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSetting: JwtOptions"));
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthService, AuthService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration();
+app.Run();
+// This method applies any pending Entity Framework Core migrations to the database at application startup.
+// It creates a service scope, retrieves the AppDbContext, checks for pending migrations, and applies them if any exist.
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            dbContext.Database.Migrate();
+        }
+    }
+}
