02e6d0aff27636b62f95c0238166424e5b097aac
Shivlal Bhattarai <sbmrc0321@gmail.com>
2025-08-29 09:05:13 +0545

    productapi and access in web app

---
+++ Mango.Service.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using Mango.Service.ProductAPI.Models;
+using Mango.Service.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Service.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public ProductAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> objects = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(objects);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product objects = _db.Products.First(u => u.ProductId == id);
+                _response.Result = _mapper.Map<ProductDto>(objects);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("GetByName/{name}")]
+        public ResponseDto GetByCode(string name)
+        {
+            try
+            {
+                Product objects = _db.Products.FirstOrDefault(u => u.Name.ToLower() == name.ToLower());
+                if (objects == null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<ProductDto>(objects);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(productDto);
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(productDto);
+                _db.Products.Update(product);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                var product = _db.Products.First(u => u.ProductId == id);
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProductAsync(string name);
+        Task<ResponseDto?> GetAllProductAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
